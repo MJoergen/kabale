08c8b148b3bc0cb38ef6f2668134e58f5bfd9b0a
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2020-05-31 21:51:52 +0200

    WIP.

--- kabale.cpp
+++ kabale.cpp
@@ -1,42 +1,50 @@
-#include <stack>
+#include <set>
 #include <algorithm>
 #include <random>
 #include <iostream>
 #include <iomanip>
+#include <assert.h>
 
 #define NUM_QUADS 13
 
 std::vector<int> cards;
 std::vector<int> piles[4];
+auto rng = std::default_random_engine {};
+
 
 static int cards_hash()
 {
-   std::size_t seed = cards.size();
-   for(auto& i : cards) {
+   int seed = cards.size();
+   for(auto i : cards) {
       seed ^= i + 0x9e3779b9 + (seed << 6) + (seed >> 2);
    }
    return seed;
-}
+} // cards_hash
 
 
 static void cards_print()
 {
    for (int i=0; i<cards.size(); ++i)
+   {
+      assert(cards[i] > 0);
       std::cout << cards[i] << " ";
+   }
    std::cout << std::endl;
-}
+} // cards_print
+
 
 static void cards_shuffle()
 {
    // Initialize deck of cards
+   cards.clear();
    for (int i=1; i<=NUM_QUADS; ++i) {
       for (int p=0; p<4; ++p) {
          cards.push_back(i);
       }
    }
-   auto rng = std::default_random_engine {};
    std::shuffle(cards.begin(), cards.end(), rng);
-}
+} // cards_shuffle
+
 
 static void piles_print()
 {
@@ -57,7 +65,8 @@ static void piles_print()
       std::cout << std::endl;
    }
    std::cout << std::endl;
-}
+} // piles_print
+
 
 static void piles_reorder()
 {
@@ -66,28 +75,34 @@ static void piles_reorder()
       again = false;
       for (int l=0; l<3; ++l) {
          int topl = piles[l].back();
-         if (topl > 0) {
-            for (int r=l+1; r<4; ++r) {
-               int topr = piles[r].back();
-               if (topl == topr) {
-                  piles[r].push_back(topl);
-                  piles[l].pop_back();
-                  topl = piles[l].back();
-                  again = true;
-               }
+         for (int r=l+1; r<4; ++r) {
+            int topr = piles[r].back();
+            if (topl > 0 && topl == topr) {
+               piles[r].push_back(topl);
+               piles[l].pop_back();
+               topl = piles[l].back();
+               again = true;
             }
          }
       }
-   } // while (again)
-}
+   }
+} // piles_reorder
 
-int main()
+
+// Returns true if game was solved.
+// Returns false if infinite loop was detected.
+static bool game_play()
 {
    cards_shuffle();
 
+   std::set<int> hashes;
+
    while (cards.size() != 0) {
-      cards_print();
-      std::cout << cards_hash() << std::endl;
+      int hash = cards_hash();
+      if (hashes.find(hash) != hashes.end()) {
+         return false;
+      }
+      hashes.insert(hash);
 
       // Reset piles.
       for (int p=0; p<4; ++p) {
@@ -99,8 +114,8 @@ int main()
       for (auto it=cards.begin(); it!=cards.end();) {
          // Check if completed row
          if (it[0] == it[1] && it[1] == it[2] && it[2] == it[3]) {
-            std::cout << "Finished card " << it[0] << std::endl;
             it += 4;
+            hashes.clear();
             // Don't push cards to piles
             continue;
          }
@@ -113,7 +128,6 @@ int main()
          piles_reorder();
       } // for
 
-//      piles_print();
       // Recreate deck from piles
       cards.clear();
       for (int p=0; p<4; p++)
@@ -121,5 +135,20 @@ int main()
          cards.insert(cards.end(), piles[p].begin()+1, piles[p].end());
       }
    } // while (true)
-}
+   return true;
+} // game_play
+
+
+
+int main()
+{
+   int solved = 0;
+   for (int i=0; i<100000; ++i)
+   {
+      bool res = game_play();
+      solved += res;
+   }
+
+   std::cout << solved << std::endl;
+} // main
 
